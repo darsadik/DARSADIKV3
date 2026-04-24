0d636e2fa7207abc41ed7126944907e43c8022ab
darsadik <abdelhafidbaadi9@gmail.com>
2026-04-24 19:27:12 +0100

    Add missing color 200 to brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ module.exports = {
         brand: {
           50:  '#e8f0fb',
           100: '#c5d8f5',
+          200: '#a0bfee',  // ← FIXED: this was missing!
+          300: '#7aa6e6',
+          400: '#4d8bdc',
           500: '#1a5fa8',
           600: '#145090',
           700: '#0d3d6e',
+          800: '#0a2d52',
           900: '#071f3a',
         }
       },
